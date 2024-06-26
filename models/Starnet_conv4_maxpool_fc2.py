249f8987c7d5f942d013085f2d1dd6e52bd09de2
ParkSeongJin99 <bluegiant99@snu.ac.kr>
2024-06-26 16:04:43 +0900

    change model - conv8 maxpool fc1

--- models/Starnet_conv4_maxpool_fc2.py
+++ models/Starnet_conv4_maxpool_fc2.py
@@ -10,7 +10,7 @@ class StarNet(nn.Module):
         self.conv3 = nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2)
         self.conv4 = nn.Conv2d(128, 256, kernel_size=5, stride=2, padding=2)
         
-        self.global_avg_pool = nn.AdaptiveMaxPool2d((1, 1))
+        self.global_max_pool = nn.AdaptiveMaxPool2d((1, 1))
         self.fc1 = nn.Linear(256, 128)
         self.fc2 = nn.Linear(128, 1)
 
@@ -21,7 +21,7 @@ class StarNet(nn.Module):
         x = F.relu(self.conv3(x))
         x = F.relu(self.conv4(x))
 
-        x = self.global_avg_pool(x)
+        x = self.global_max_pool(x)
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
---
+++ models/Starnet_conv8_maxpool_fc1.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class StarNet(nn.Module):
+    def __init__(self):
+        super(StarNet, self).__init__()
+        self.conv1 = nn.Conv2d(2, 32, kernel_size=5, stride=1, padding=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=2)
+        self.conv6 = nn.Conv2d(64, 128, kernel_size=5, stride=2, padding=2)
+        self.conv6_1 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.conv6_2 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
+        self.conv6_3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.conv6_4 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)
+        self.conv6_5 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        
+        self.global_max_pool = nn.AdaptiveMaxPool2d((1, 1))
+        self.fc1 = nn.Linear(512, 1)
+        
+
+    
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv6(x))
+        x = F.relu(self.conv6_1(x))
+        x = F.relu(self.conv6_2(x))
+        x = F.relu(self.conv6_3(x))
+        x = F.relu(self.conv6_4(x))
+        x = F.relu(self.conv6_5(x))
+
+        x = self.global_max_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        
+
+        return x
